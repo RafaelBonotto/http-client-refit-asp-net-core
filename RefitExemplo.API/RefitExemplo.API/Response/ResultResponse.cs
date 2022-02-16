0524ca30dd539083edb257df9dc00d096c656e5e
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2022-02-15 22:42:43 -0300

    Create ResultResponse.cs

---
+++ RefitExemplo.API/RefitExemplo.API/Response/ResultResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace RefitExemplo.API.Response
+{
+    public class ResultResponse
+    {
+        public int StatusCode { get; set; }
+        public string Message { get; set; }
+        public bool IsOk { get; set; }
+        public object Data { get; set; }
+    }
+}

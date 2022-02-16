ddb6ee289d86b367816de3e3997ea4ff462b9b07
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2022-02-15 22:42:27 -0300

    Create IEnderecoRepository.cs

---
+++ RefitExemplo.API/RefitExemplo.API/Interfaces/IEnderecoRepository.cs
@@ -0,0 +1,15 @@
+﻿using Refit;
+using RefitExemplo.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RefitExemplo.API.Interfaces
+{
+    public interface IEnderecoRepository
+    {
+        [Get("/v1/endereco/{cep}")]
+        Task<EnderecoModel> ReturnEndereco(string cep);
+    }
+}

60d88edec8bdfd254f2e0494ebf85552d90386d0
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2022-02-15 22:42:15 -0300

    Create EnderecoController.cs

---
+++ RefitExemplo.API/RefitExemplo.API/Controllers/EnderecoController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RefitExemplo.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EnderecoController : ControllerBase
+    {
+
+    }
+}

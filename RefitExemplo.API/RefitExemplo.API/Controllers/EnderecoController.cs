f926cdb14d04c00361d1169cbb7cb9e081fbf78a
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2022-02-20 23:11:25 -0300

    Exemplo2

--- RefitExemplo.API/RefitExemplo.API/Controllers/EnderecoController.cs
+++ RefitExemplo.API/RefitExemplo.API/Controllers/EnderecoController.cs
@@ -1,18 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Refit;
 using RefitExemplo.API.Interfaces;
 using RefitExemplo.API.Models;
 using System.Threading.Tasks;
 
 namespace RefitExemplo.API.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/v1/[controller]")]
     [ApiController]
     public class EnderecoController : ControllerBase
     {
-        [HttpGet]        
-        public async Task<EnderecoModel> GetEndereco([FromServices] IEnderecoRepository repository, [FromQuery] string cep)
+        [HttpGet("exemplo1")]        
+        public async Task<EnderecoModel> GetEnderecoExemplo1([FromServices] IEnderecoRepository repository, [FromQuery] string cep)
         {
             return await repository.ReturnEndereco(cep);
+        }
+
+        [HttpGet("exemplo2")]
+        public async Task<EnderecoModel> GetEnderecoExemplo2([FromQuery] string cep)
+        {
+            var urlBase = "https://viacep.com.br";
+            var api = RestService.For<IEnderecoRepository>(urlBase);
+             
+            return await api.ReturnEnderecoExemplo2(cep); 
         }
     }
 }
